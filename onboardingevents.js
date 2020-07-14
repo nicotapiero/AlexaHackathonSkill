ac979d33488552f455c0ea079a1215cc342343c8
Nico Tapiero <47359303+nicotapiero@users.noreply.github.com>
2020-07-13 23:39:51 -0400

    Create onboardingevents.js

---
+++ onboardingevents.js
@@ -0,0 +1,9 @@
+/* eslint-disable  func-names */
+/* eslint-disable max-len */
+/* eslint quote-props: ['error', 'consistent'] */
+// TODO: Replace this data with your own.
+module.exports = {
+    'background check': 'You can expect a background check to be initated 30 to 60 days before your start date.',
+    'introduction from my manager': 'You can expect an introduction from your manager via email to come 20 to 30 days before your start date.',
+    'i-9 verification': 'MyDocs will send you important paperwork to complete including your I-9 verification 10 to 20 days before you start date.'
+};
